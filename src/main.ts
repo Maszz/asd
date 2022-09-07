bbbbeb9a0b49abdf0cbce009a79d6c00893a69a0
Mawin Sukmongkol <mawinsukmongkol@Mawins-MacBook-Air.local>
2022-09-07 12:02:18 +0700

    add cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ForbiddenException } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const whitelist = [
+    'localhost:3000',
+    'localhost:3001',
+    'http://localhost:4200',
+  ];
+  app.enableCors({
+    origin: function (origin, callback) {
+      if (whitelist.indexOf(origin) !== -1 || origin == undefined) {
+        console.log(`${origin}` + ' is allowed');
+        callback(null, true);
+      } else {
+        console.log(`${origin} is not allowed`);
+        callback(new ForbiddenException('Not allowed by CORS'));
+      }
+    },
+    credentials: true,
+  });
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
